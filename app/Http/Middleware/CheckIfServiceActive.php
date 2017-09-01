2123f70791efc9351d92a221b86e9361ef6de32e
Victor Planchuelo <victorplanchuelo@gmail.com>
2017-09-01 11:13:56 +0200

    - Corrección error por el cual si no era una llamada de servicio, pero afectaba a un servicio (por ejemplo, guardar la nota de un mensaje), el middleware devolvía un error

--- app/Http/Middleware/CheckIfServiceActive.php
+++ app/Http/Middleware/CheckIfServiceActive.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 use App\Service as Service;
+use Illuminate\Support\Facades\Log;
 
 class CheckIfServiceActive
 {
@@ -18,14 +19,21 @@ class CheckIfServiceActive
     public function handle($request, Closure $next)
     {
     	//Aqui debemos comprobar si el usuario tiene el servicio activo
-	    $id_service = Service::where('path', $request->path())->first()->id;
-	    $user_service = Auth::user()->services()->where('id', $id_service)->first();
+	    Log::info(Service::where('path', $request->path())->count());
+	    if(Service::where('path', $request->path())->count()>0)
+	    {
+		    $id_service = Service::where('path', $request->path())->first()->id;
+		    $user_service = Auth::user()->services()->where('id', $id_service)->first();
 
-	    if(count($user_service)>0 && $user_service->pivot->active===1)
-	    	return $next($request);
+		    if(count($user_service)>0 && $user_service->pivot->active===1)
+			    return $next($request);
 
-	    //QUEDARIA POR VER COMO HACER ESTO (LLAMAMOS A BACK CON UN ERROR O LLAMAMOS A UNA VISTA DE ERROR)
-	    // return response('Unauthorized.', 401);
-	    return redirect()->back()->withErrors(['error' => 'No tienes activado este servicio']);
+		    //QUEDARIA POR VER COMO HACER ESTO (LLAMAMOS A BACK CON UN ERROR O LLAMAMOS A UNA VISTA DE ERROR)
+		    // return response('Unauthorized.', 401);
+		    return redirect()->back()->withErrors(['error' => 'No tienes activado este servicio']);
+	    }
+	    else
+		    return $next($request);
+
     }
 }
